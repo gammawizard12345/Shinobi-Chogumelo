4a2cdc5c7c15d90f130603878fd0a3ca0e674449
gammawizard12345 <162822629+gammawizard12345@users.noreply.github.com>
2024-09-16 10:24:02 -0300

    Primeiro Upload

---
+++ Shinobi Chogumel/Assets/Scripts/GameManager.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    SceneManager sceneManager;
+  
+   
+        
+    public GameObject MenuScreen;
+    public GameObject MainMenu;
+    public GameObject Sphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene Scene0 = SceneManager.GetSceneByName("MainMenu");
+        MenuScreen.SetActive(false);
+        Scene Scene1 = SceneManager.GetSceneByName("Gameplay");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        if (SceneManager.GetActiveScene().name == "Gameplay")
+        {
+            
+
+            for (int i = 0; i < 101; i++)
+            {
+                instantiateEnemy ();
+            }
+        }
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        MenuScreen.SetActive(false );
+        MainMenu.SetActive(true);
+    }
+
+    public void Menu()
+    {
+        MenuScreen.SetActive(true);
+    }
+    public  void VoltarJogar()
+    {
+        MenuScreen.SetActive(false);
+    }
+
+    public void ToGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+        
+
+        
+    }
+
+     void instantiateEnemy()
+    {
+        
+
+
+
+
+
+    }
+
+    
+
+}
